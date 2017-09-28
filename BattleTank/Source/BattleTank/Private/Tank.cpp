2392fa9945e4226d59bd076f16534096359edd95
Harrier-GR7\Drew <buiscuitzmonster@gmail.com>
2017-09-28 22:42:22 +0200

    Section 4 Lecture196

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -3,6 +3,11 @@
 #include "Tank.h"
 #include "Engine/World.h"
 
+float ATank::GetHealthPercent() const
+{
+	return (float)CurrentHealth / (float)StartingHealth;
+}
+
 // Sets default values
 ATank::ATank()
 {
